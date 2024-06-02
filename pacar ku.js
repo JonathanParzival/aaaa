556c8aaa1dc2dfc14076c06c272c5da7bc018b27
JonathanParzival <93761800+JonathanParzival@users.noreply.github.com>
2024-06-02 20:15:40 +0700

    dsdsd
    aasdsd

---
+++ pacar ku.js
@@ -0,0 +1,8 @@
+// Ubah nama pacar di sini
+const namaPacar = "Gabrielle keisha";
+
+// Mengambil elemen HTML dengan id 'name'
+const nameElement = document.getElementById('name');
+
+// Mengubah teks elemen dengan nama pacar
+nameElement.textContent = namaPacar;
